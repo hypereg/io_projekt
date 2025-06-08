5a9650bd77ac2ea49d7359a3ed870aec7d191ef2
Rafał Grzelak <rafikg523@gmail.com>
2025-06-09 00:59:39 +0200

    add plan data

---
+++ app/src/main/java/com/example/myapplication/ui/plan/ZajeciaDataProvider.java
@@ -0,0 +1,55 @@
+package com.example.myapplication.ui.plan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZajeciaDataProvider {
+    public static List<Zajecie> getPlanData() {
+        List<Zajecie> planData = new ArrayList<>();
+
+        planData.add(new Zajecie("Transmisja Danych", "A", "Prof Kowalski", "8:15", "10:00", "W1 - 220", "02.06"));
+        planData.add(new Zajecie("Systemy Operacyjne", "L", "Dr Nowak", "10:15", "12:00", "W1 - 215", "02.06"));
+        planData.add(new Zajecie("Matematyka Dyskretna", "W", "Dr Maj", "8:15", "9:45", "W1 - 101", "03.06"));
+        planData.add(new Zajecie("Algorytmy i Struktury Danych", "C", "Dr Nowak", "10:00", "11:30", "W1 - 120", "03.06"));
+        planData.add(new Zajecie("Fizyka", "W", "Prof Kowalski", "12:00", "13:30", "W2 - 201", "03.06"));
+        planData.add(new Zajecie("Język Angielski", "L", "Mgr Smith", "8:00", "9:30", "W1 - 110", "04.06"));
+        planData.add(new Zajecie("Programowanie Obiektowe", "L", "Dr Nowak", "10:00", "11:30", "W1 - 119", "04.06"));
+        planData.add(new Zajecie("Systemy Wbudowane", "L", "Dr Nowak", "12:00", "13:30", "W1 - 215", "04.06"));
+        planData.add(new Zajecie("Sieci Komputerowe", "W", "Prof Kowalski", "8:15", "9:45", "W1 - 220", "05.06"));
+        planData.add(new Zajecie("Grafika Komputerowa", "L", "Dr Maj", "10:00", "11:30", "W1 - 101", "05.06"));
+        planData.add(new Zajecie("Sztuczna Inteligencja", "W", "Dr Nowak", "12:00", "13:30", "W1 - 120", "05.06"));
+        planData.add(new Zajecie("Matematyka", "W", "Dr Maj", "8:15", "9:45", "W1 - 101", "06.06"));
+        planData.add(new Zajecie("Programowanie", "C", "Dr Nowak", "10:00", "11:30", "W1 - 120", "06.06"));
+        planData.add(new Zajecie("Bazy Danych", "L", "Prof Nowak", "12:00", "13:30", "W1 - 119", "06.06"));
+        planData.add(new Zajecie("Fizyka", "W", "Prof Kowalski", "8:15", "9:45", "W2 - 201", "07.06"));
+        planData.add(new Zajecie("Systemy Operacyjne", "L", "Dr Nowak", "10:00", "11:30", "W1 - 215", "07.06"));
+        planData.add(new Zajecie("Transmisja Danych", "A", "Prof Kowalski", "12:00", "13:30", "W1 - 220", "07.06"));
+        planData.add(new Zajecie("Język Niemiecki", "L", "Mgr Schmidt", "8:00", "9:30", "W1 - 110", "08.06"));
+        planData.add(new Zajecie("Matematyka Dyskretna", "W", "Dr Maj", "10:00", "11:30", "W1 - 101", "08.06"));
+        planData.add(new Zajecie("Algorytmy i Struktury Danych", "C", "Dr Nowak", "8:15", "9:45", "W1 - 120", "09.06"));
+        planData.add(new Zajecie("Fizyka", "W", "Prof Kowalski", "10:00", "11:30", "W2 - 201", "09.06"));
+        planData.add(new Zajecie("Systemy Wbudowane", "L", "Dr Nowak", "12:00", "13:30", "W1 - 215", "09.06"));
+        planData.add(new Zajecie("Sieci Komputerowe", "W", "Prof Kowalski", "14:00", "15:30", "W1 - 220", "09.06"));
+        planData.add(new Zajecie("Grafika Komputerowa", "L", "Dr Maj", "8:15", "9:45", "W1 - 101", "10.06"));
+        planData.add(new Zajecie("Sztuczna Inteligencja", "W", "Dr Nowak", "10:00", "11:30", "W1 - 120", "10.06"));
+        planData.add(new Zajecie("Język Angielski", "L", "Mgr Smith", "12:00", "13:30", "W1 - 110", "10.06"));
+        planData.add(new Zajecie("Matematyka", "W", "Dr Maj", "8:15", "9:45", "W1 - 101", "11.06"));
+        planData.add(new Zajecie("Programowanie", "C", "Dr Nowak", "10:00", "11:30", "W1 - 120", "11.06"));
+        planData.add(new Zajecie("Bazy Danych", "L", "Prof Nowak", "12:00", "13:30", "W1 - 119", "11.06"));
+        planData.add(new Zajecie("Fizyka", "W", "Prof Kowalski", "14:00", "15:30", "W2 - 201", "11.06"));
+        planData.add(new Zajecie("Systemy Operacyjne", "L", "Dr Nowak", "8:15", "9:45", "W1 - 215", "12.06"));
+        planData.add(new Zajecie("Transmisja Danych", "A", "Prof Kowalski", "10:00", "11:30", "W1 - 220", "12.06"));
+        planData.add(new Zajecie("Język Niemiecki", "L", "Mgr Schmidt", "12:00", "13:30", "W1 - 110", "12.06"));
+        planData.add(new Zajecie("Matematyka Dyskretna", "W", "Dr Maj", "8:15", "9:45", "W1 - 101", "13.06"));
+        planData.add(new Zajecie("Algorytmy i Struktury Danych", "C", "Dr Nowak", "10:00", "11:30", "W1 - 120", "13.06"));
+        planData.add(new Zajecie("Fizyka", "W", "Prof Kowalski", "12:00", "13:30", "W2 - 201", "13.06"));
+        planData.add(new Zajecie("Język Angielski", "L", "Mgr Smith", "8:00", "9:30", "W1 - 110", "14.06"));
+        planData.add(new Zajecie("Programowanie Obiektowe", "L", "Dr Nowak", "10:00", "11:30", "W1 - 119", "14.06"));
+        planData.add(new Zajecie("Systemy Wbudowane", "L", "Dr Nowak", "12:00", "13:30", "W1 - 215", "14.06"));
+        planData.add(new Zajecie("Sieci Komputerowe", "W", "Prof Kowalski", "8:15", "9:45", "W1 - 220", "15.06"));
+        planData.add(new Zajecie("Grafika Komputerowa", "L", "Dr Maj", "10:00", "11:30", "W1 - 101", "15.06"));
+        planData.add(new Zajecie("Sztuczna Inteligencja", "W", "Dr Nowak", "12:00", "13:30", "W1 - 120", "15.06"));
+
+        return planData;
+    }
+}
